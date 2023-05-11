c2636b94f36a8143c639646a6755e5c74bd4354f
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-11 14:42:36 +0200

    controller  e model creati

--- app/Http/Controllers/Myfolder/MyController.php
+++ app/Http/Controllers/Myfolder/MyController.php
@@ -8,5 +8,8 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    //
+    public function index(){
+
+        return view('home');
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'My Movie list')
+
+@section('content')
+  <h1>ciaone</h1>
+@endsection
