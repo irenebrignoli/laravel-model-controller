6c57a9f3322af7396bad19e9b3f11afd0e5b844f
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-11 14:29:56 +0200

    first commit

---
+++ app/Http/Controllers/Myfolder/MyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Myfolder;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    //
+}
